77c00a6fcc67f399e524bcdd330285541d4e37ae
zeyuanzhao <14340342+zeyuanzhao@users.noreply.github.com>
2025-04-01 10:44:15 -0400

    nextui init

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,17 @@
+const { heroui } = require("@heroui/theme");
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
-
     // Or if using `src` directory:
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  darkMode: "class",
+  plugins: [heroui()],
 };
