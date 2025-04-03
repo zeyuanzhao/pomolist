c64c2fd3d525752fcb65fb3c3de77989ee12b86a
zeyuanzhao <14340342+zeyuanzhao@users.noreply.github.com>
2025-04-02 23:19:56 -0400

    redirect to auth/app if logged in

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,4 +1,19 @@
-const AppLayout = ({ children }: { children: React.ReactNode }) => {
+import { createClient } from "@/utils/supabase/server";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
+
+const AppLayout = async ({ children }: { children: React.ReactNode }) => {
+  const pathname = (await headers()).get("x-next-pathname");
+  const supabase = await createClient();
+
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  if (user && !pathname?.includes("logout")) {
+    redirect("/app");
+  }
+
   return (
     <div className="flex flex-col w-screen h-screen text-s">{children}</div>
   );
--- app/app/layout.tsx
+++ app/app/layout.tsx
@@ -1,6 +1,20 @@
 import { SideBar } from "@/components/SideBar";
+import { createClient } from "@/utils/supabase/server";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
 
-const AppLayout = ({ children }: { children: React.ReactNode }) => {
+const AppLayout = async ({ children }: { children: React.ReactNode }) => {
+  const pathname = (await headers()).get("x-next-pathname");
+  const supabase = await createClient();
+
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  if (!user) {
+    redirect("/login");
+  }
+
   return (
     <div className="flex flex-row w-screen h-screen text-ts">
       <SideBar />
