dc7ab2c3be4581f4ebeec95513df01d6034a1084
zeyuanzhao <14340342+zeyuanzhao@users.noreply.github.com>
2025-04-03 21:51:17 -0400

    sync tasks

--- components/TasksList/index.tsx
+++ components/TasksList/index.tsx
@@ -1,9 +1,33 @@
+"use client";
+
 import { TaskInfo } from "@/interfaces";
+import { createClient } from "@/utils/supabase/client";
+import { useEffect, useState } from "react";
 
 export const TasksList = ({ initialTasks }: { initialTasks?: TaskInfo[] }) => {
+  const supabase = createClient();
+  const [tasks, setTasks] = useState<TaskInfo[]>(initialTasks || []);
+
+  useEffect(() => {
+    const channel = supabase
+      .channel("tasks")
+      .on(
+        "postgres_changes",
+        { event: "INSERT", schema: "public", table: "tasks" },
+        (payload) => {
+          setTasks((prevTasks) => [...prevTasks, payload.new as TaskInfo]);
+        }
+      )
+      .subscribe();
+
+    return () => {
+      supabase.removeChannel(channel);
+    };
+  }, []);
+
   return (
     <div>
-      {initialTasks?.map((task) => (
+      {tasks?.map((task) => (
         <div key={task.id} className="flex flex-row items-center">
           <p>{task.name}</p>
         </div>
