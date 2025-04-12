c431a54f417011df8d8a6d353e141ff8ce96b802
zeyuanzhao <14340342+zeyuanzhao@users.noreply.github.com>
2025-04-11 23:33:42 -0400

    today page formatting

--- app/app/tasks/page.tsx
+++ app/app/tasks/page.tsx
@@ -4,6 +4,7 @@ import { TasksList } from "@/components/TasksList";
 import { UserError } from "@/components/UserError";
 import { TaskInfo, taskSchema } from "@/interfaces";
 import { createClient } from "@/utils/supabase/server";
+import { ScrollShadow } from "@heroui/scroll-shadow";
 
 const TasksPage = async () => {
   const supabase = await createClient();
@@ -17,12 +18,14 @@ const TasksPage = async () => {
 
   return (
     <div className="flex-1 flex flex-row justify-center">
-      <div className="flex-1 max-w-4xl pt-8 flex flex-col">
+      <div className="flex-1 max-w-4xl pt-16 flex flex-col">
         <div>
           <h1 className="text-3xl font-bold">Tasks</h1>
         </div>
-        <div>
-          <TasksList />
+        <div className="flex-1 pt-8">
+          <ScrollShadow className="w-full max-h-[calc(100vh-14rem)]">
+            <TasksList />
+          </ScrollShadow>
         </div>
       </div>
     </div>
